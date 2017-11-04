7953e9475ac6d8b37bb1d1ef9b4f864e797251db
Juan Manuel Vicente <juanma.v82@gmail.com>
2017-11-04 14:25:58 -0300

    a desplegar en digital ocean

--- twitter-clon-front/src/components/Tweet/TweetForm.js
+++ twitter-clon-front/src/components/Tweet/TweetForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import config from '../../config';
 
 class TweetForm extends React.Component {
 
@@ -11,12 +12,36 @@ class TweetForm extends React.Component {
 
     save = () => {
         console.log(this.state);
+
+        fetch(config.apiUrl +'tweets', {
+            headers :{
+                "Content-Type" : "application/json",
+                "x-access-token": localStorage.getItem('token')
+            },
+            body: JSON.stringify(this.state),
+            method: 'POST'
+        })
+            .then(response => {
+                if (response.status == 201){
+                    // creatdo con exito!
+                    alert('Creado con exito!!!!!');
+                    window.location.href = '/';
+                } else {
+                    alert('No se pudo generar el tweet!');
+                }
+            })
+    };
+
+    handleMessage = (event) => {
+        this.setState({ message: event.target.value})
     };
 
     render(){
+        console.log(this.state);
         return (
             <div>
-                Formulario de creacion!
+                message: <input type='text' onChange={this.handleMessage}/>
+                <button onClick={this.save}> Crear! </button>
             </div>
         )
     }
